2cad955adc865e754b30eeab2eaa491d3c9a98b5
reearming <reearming92@gmail.com>
2026-09-09 21:48:26 +0200

    whole english

--- kernel/main.c
+++ kernel/main.c
@@ -33,10 +33,12 @@ void put_pixel(uint32_t x, uint32_t y, uint32_t color, struct limine_framebuffer
 void put_char(uint32_t x, uint32_t y, uint32_t color, struct limine_framebuffer fb, const uint8_t letter[])
 {
     uint32_t *pixpoint = (uint32_t *)fb.address;
+    
+    x += 7;
 
     for (uint32_t i = 0; i < 8; i++)
     { 
-        for (uint32_t j = 8; j > 0; j--)
+        for (int j = 7; j >= 0; j--)
         {
             if (letter[i]>>j & 1)
             {
@@ -61,8 +63,24 @@ void kmain(void) {
         }
     }*/
 
-    put_char(50, 65, 0xFFFFFF, *framebuffer, Z);
-    put_char(60, 65, 0xFFFFFF, *framebuffer, Z);
+    put_char(50, 65, 0xFFFFFF, *framebuffer, H);
+    put_char(59, 65, 0xFFFFFF, *framebuffer, E);
+    put_char(68, 65, 0xFFFFFF, *framebuffer, L);
+    put_char(77, 65, 0xFFFFFF, *framebuffer, L);
+    put_char(86, 65, 0xFFFFFF, *framebuffer, O);
+    put_char(95, 65, 0xFFFFFF, *framebuffer, Comma);
+    put_char(104, 65, 0xFFFFFF, *framebuffer, Empty);
+    put_char(113, 65, 0xFFFFFF, *framebuffer, I);
+    put_char(122, 65, 0xFFFFFF, *framebuffer, Empty);
+    put_char(131, 65, 0xFFFFFF, *framebuffer, A);
+    put_char(140, 65, 0xFFFFFF, *framebuffer, M);
+    put_char(149, 65, 0xFFFFFF, *framebuffer, Empty);
+    put_char(158, 65, 0xFFFFFF, *framebuffer, K);
+    put_char(167, 65, 0xFFFFFF, *framebuffer, E);
+    put_char(186, 65, 0xFFFFFF, *framebuffer, R);
+    put_char(195, 65, 0xFFFFFF, *framebuffer, N);
+    put_char(204, 65, 0xFFFFFF, *framebuffer, E);
+    put_char(213, 65, 0xFFFFFF, *framebuffer, L);
 
     for (;;) { }
 }
